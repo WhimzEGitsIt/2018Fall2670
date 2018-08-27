f9295b51a39e16c665b2a28a53043dd4dc1d9b05
Chad Green <green.chad22@gmail.com>
2018-08-27 14:28:58 -0600

    Added New Scene, Added EventSystem

---
+++ 2670Fall/Assets/Scripts/EventScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventScript : MonoBehaviour
+{
+
+	public UnityEvent MyEvent;
+
+	private void OnMouseDown()
+	{
+		MyEvent.Invoke();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		MyEvent.Invoke();
+	}
+	
+	
+	//private void Start()
+	//{
+		//MyEvent.Invoke();
+	//}
+	
+
+}
