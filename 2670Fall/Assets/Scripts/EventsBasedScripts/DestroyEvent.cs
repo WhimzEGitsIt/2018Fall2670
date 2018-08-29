ce10de3097611cbd53f05761beb45a48841420a6
Chad Green <green.chad22@gmail.com>
2018-08-29 14:31:19 -0600

    8/29 Inclass Stuff

---
+++ 2670Fall/Assets/Scripts/EventsBasedScripts/DestroyEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DestroyEvent : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	private void OnDestroy()
+	{
+		Event.Invoke();
+	}
+	
+}
