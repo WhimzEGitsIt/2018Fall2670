f9716e0e10582d90110557609ea4d62b18e58e70
Unknown <green.chad22@gmail.com>
2018-09-20 23:40:31 -0600

    Game having some problems, will not give up!

---
+++ [0] Assignment/Assets/Scripts/FloatData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Float", menuName = "CG/floatdata")]
+
+public class FloatData : ScriptableObject
+{
+
+	public float value;
+	
+	public virtual float Value
+	{
+		get { return value;}
+	}
+
+}
---
+++ [0] Assignment/Assets/Scripts/PrefabData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class PrefabData : ScriptableObject
+{
+
+	public GameObject Prefab;
+	public Sprite SpriteImage;
+	public Color SpriteColor;
+
+	public void OnConfiger()
+	{
+		var newPrefab = Instantiate(Prefab);
+		var newSprite = newPrefab.GetComponent<SpriteRenderer>();
+		newSprite.sprite = SpriteImage;
+		newSprite.color = SpriteColor;
+	}
+
+}
