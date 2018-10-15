43c1badcc71f69ff29c0e4a8101ebbe0cd3b7bbe
Chad Green <green.chad22@gmail.com>
2018-10-15 14:43:10 -0600

    Learned how to make a store

---
+++ 2670Fall/Assets/HyperCasual/HyperGameScript/Objects.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Objects", menuName = "Store/Objects")]
+public class Objects : ScriptableObject, IEnumerable
+{
+	public List<Object> ObjectList;
+	public IEnumerator GetEnumerator()
+	{
+		throw new System.NotImplementedException();
+	}
+}
---
+++ 2670Fall/Assets/HyperCasual/HyperGameScript/Store.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Store", menuName = "Store/StoreFront")]
+public class Store : ScriptableObject
+{
+    public Objects Purchased;
+    public Objects Available;
+
+    public void MakePurchase(Object obj)
+    {
+        foreach (var availableObject in Purchased.ObjectList)
+        {
+            if (availableObject == obj)
+            {
+                Purchased.ObjectList.Add(obj);
+                Available.ObjectList.Remove(availableObject);
+            }
+        }
+    }
+    
+}
