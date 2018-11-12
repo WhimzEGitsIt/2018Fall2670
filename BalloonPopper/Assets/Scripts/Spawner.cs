26636a004524fc42cff192be7be16fb81c3c2b7f
Chad Green <green.chad22@gmail.com>
2018-11-12 14:25:56 -0700

    A lot of work done on Balloon popper

---
+++ BalloonPopper/Assets/Scripts/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+	public Transform[] spawnPoints;
+
+	public GameObject balloonPrefab;
+
+	public float timeBetweenSpawn = 1f;
+	
+	private float timetoSpawn = 2f;
+
+	
+	// Use this for initialization
+	void Update ()
+	{
+
+		if (Time.time >= timetoSpawn)
+		{
+			SpawnBlocks();
+			timetoSpawn = Time.time + timeBetweenSpawn;
+		}
+		
+		
+		
+	}
+
+	void SpawnBlocks()
+	{
+		int randomIndex = Random.Range(0, spawnPoints.Length);
+
+		for (int i = 0; i < spawnPoints.Length; i++)
+		{
+			if (randomIndex == i)
+			{
+				Instantiate(balloonPrefab, spawnPoints[i].position, Quaternion.identity);
+			}
+		}
+	}
+	
+	
+}
