dcaac4344ef57a5daa94ebe728e7978b1fc02c56
Chad Green <green.chad22@gmail.com>
2018-12-10 00:29:06 -0700

    MoreBalloonHealthCrap

---
+++ BalloonPopper/Assets/Scripts/HUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+
+	public Sprite[] HeartSprites;
+
+	public Image HeartUI;
+
+	private HealthPlayer health;
+
+	void Start()
+	{
+		health = GameObject.FindGameObjectWithTag("Grandma").GetComponent<HealthPlayer>();
+	}
+
+	void Update()
+	{
+		HeartUI.sprite = HeartSprites [health.curHealth];
+	}
+	
+
+
+}
