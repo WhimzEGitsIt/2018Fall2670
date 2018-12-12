26cfbbcf6e317794f1e847e3b7a14bb97d652124
Chad Green <green.chad22@gmail.com>
2018-12-11 21:22:30 -0700

    Added Umbrella Started On Store

---
+++ BalloonPopper/Assets/Scripts/IntData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SingleVars/IntData")]
+public class IntData : ScriptableObject
+{
+    public int Value;
+}
---
+++ BalloonPopper/Assets/Scripts/Store.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Store : ScriptableObject
+{
+
+	public Object Item;
+	public List<Object> Purchased;
+	public int ItemValue;
+	public IntData Cash;
+	
+	public void Purchase()
+	{
+		if (Cash.Value < ItemValue) return;
+		Purchased.Add(Item);
+		Cash.Value -= ItemValue;
+	}
+
+	public void AddObjectToGame()
+	{
+		if (Purchased.Count <= 0) return;
+		Instantiate(Purchased[0]);
+		Purchased.RemoveAt(0);
+	}
+	
+	
+}
